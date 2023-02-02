b99ee471b09c6150bc42d915de9369299903bd28
Francisco Percara <frankestudios71@gmail.com>
2023-02-02 11:19:46 -0300

    Added progress

--- videoDownloader.py
+++ videoDownloader.py
@@ -1,6 +1,18 @@
 from pytube import YouTube
-def Descarga():
-    video = YouTube(input('Entra el link del video '))
+import pytube.request
+
+pytube.request.default_range_size = 500000
+
+
+def progress_func(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    pct_completed = bytes_downloaded / total_size * 100
+    print(f'Status: {round(pct_completed, 2)}%')
+
+
+def download():
+    video = YouTube(input('Entra el link del video '), on_progress_callback=progress_func)
     video = video.streams.get_highest_resolution()
     try:
         print('El titulo del video es: ' + video.title)
@@ -9,11 +21,10 @@ def Descarga():
         print('Hubo un error en la descarga')
     print('Descarga completada')
 
-termine = False
-while(termine == False):
-    Descarga()
-    opcion = input('Continuar(Y/N)')
-    if( opcion == 'N' or opcion == 'n'):
-        termine = True
-else:
-    input()
+
+finished = False
+while not finished:
+    download()
+    option = input('Continuar(Y/N)')
+    if option == 'N' or option == 'n':
+        finished = True
